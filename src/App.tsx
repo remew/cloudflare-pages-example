1b1a993904ef22876b1162c29e0540dd7fcf43f5
remew <foolishremew@gmail.com>
2024-01-23 02:31:37 +0900

    add ssr sample

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello, react world</h1>
+    </div>
+  )
+}
--- src/_worker.ts
+++ src/_worker.ts
@@ -1,5 +1,8 @@
 import { Hono } from 'hono'
+import { renderToReadableStream } from 'react-dom/server'
 import json from './dummy.json'
+import { App } from './App.tsx'
+import { createElement } from 'react'
 
 const app = new Hono()
 
@@ -8,6 +11,13 @@ app.get('/', (c) => {
 })
 app.get('/json', (c) => {
   return c.text(JSON.stringify(json, null, 2))
+})
+app.get('/react', async (c) => {
+  const stream = await renderToReadableStream(createElement(App))
+
+  return new Response(stream, {
+    headers: { 'content-type': 'text/html' },
+  })
 })
 
 export default app
