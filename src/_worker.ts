2435513888f847a2842957ddf496dc4efbd19e56
remew <foolishremew@gmail.com>
2024-01-23 00:22:09 +0900

    add json route

--- src/_worker.ts
+++ src/_worker.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
+import json from './dummy.json'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+app.get('/json', (c) => {
+  return c.text(JSON.stringify(json, null, 2))
 })
 
 export default app
